63f5f292cefbcf69599899edb17354098c7a8e68
samrocker <samrockerislive04@gmail.com>
2024-08-06 04:43:35 +0530

    design layout complete auth pending

---
+++ app/(root)/constants/index.ts
@@ -0,0 +1,31 @@
+export const SidebarLinks = [
+    {
+        label: 'Home',
+        route: '/',
+        imgUrl: '/icons/Home.svg',
+    },
+
+    {
+        label: 'Upcoming',
+        route: '/upcoming',
+        imgUrl: '/icons/upcoming.svg',
+    },
+
+    {
+        label: 'Previous',
+        route: '/previous',
+        imgUrl: '/icons/Previous.svg',
+    },
+
+    {
+        label: 'Recordings',
+        route: '/recordings',
+        imgUrl: '/icons/Recordings.svg',
+    },
+
+    {
+        label: 'Personal Room',
+        route: '/personal-room',
+        imgUrl: '/icons/add-personal.svg',
+    }   
+]
